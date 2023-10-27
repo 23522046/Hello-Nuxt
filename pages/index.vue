1828626eee684a52afb75bf03733d608496b8aff
Radinal Dwiki Novendra, M.T <23522046@std.stei.itb.ac.id>
2023-10-27 21:35:48 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed corrupti iusto molestias quae amet saepe veniam, natus voluptatum minima, quod a aperiam accusantium ipsam distinctio, sunt vel doloremque sint eius?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique nemo odit, nihil quas dolore ipsum? Rem porro eaque ullam ducimus voluptates dolore sint nesciunt ad, veritatis laudantium temporibus ipsam odit.</p>
+
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style lang="scss" scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
